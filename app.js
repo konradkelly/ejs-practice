5e553cdc20b39ca15e7015c8dfd3099aa9d34d08
konradkelly <konradky@gmail.com>
2025-11-04 11:40:26 -0800

    instantiated person object

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const PORT = 3020;
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+	const person = { name: "Donald Duck", occupation: "plumber" };
+	res.render("home", { person });
 });
 
-app.listen(PORT, () => console.log(`Server is running on https://localhost:${PORT}`));
-
+app.listen(PORT, () =>
+	console.log(`Server is running on http://localhost:${PORT}`)
+);
